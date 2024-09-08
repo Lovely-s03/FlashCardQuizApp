8bba9c2fb28016fb92bafe9bb44b7c3c19436243
Lovely-s03 <supriyalovely0306@gmail.com>
2024-09-08 22:29:21 +0530

    QuizApp

---
+++ flashCard_QuizApp/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Quiz from './components/Quiz/Quiz'
+
+const App = () => {
+  return (
+    <div>
+      <Quiz/>
+    </div>
+  )
+}
+
+export default App
